fc0724d43997e672394eb11736a067dba724e36b
Maxym Zviahin <maxzvyagin@gmail.com>
2025-04-03 23:07:34 +0300

    redacted_in_homework_2_addet fun  for sorting_and_added_if_function

--- homework_2.py
+++ homework_2.py
@@ -1,8 +1,13 @@
 import random
 
 def get_numbers_ticket(min, max, quantity):
-    range_choice = random.sample(range(min, max), quantity)
-    print(range_choice)
+    if min >= 1 and max <= 1000:
+        range_choice = random.sample(range(min, max), quantity)#Генерує послідовність чисел у вказаному діапазоні без повторень
+        sorted_choice = sorted(range_choice)#Сортує числа у зростаючому порядку
+        return sorted_choice #Повертає результат 
+    elif min <= 0 and max >= 1001:
+        return list()#В разі некоректності генерує пустий список
+    
 
 
-get_numbers_ticket(1, 49, 6)
+print(get_numbers_ticket(1, 1000, 6))
