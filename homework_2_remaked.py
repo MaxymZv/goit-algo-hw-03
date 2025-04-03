34cde89b45dc0514a0651ee7a4c1225b335f6cca
Maxym Zviahin <maxzvyagin@gmail.com>
2025-04-04 00:35:11 +0300

    add_coments

--- homework_2_remaked.py
+++ homework_2_remaked.py
@@ -1,11 +1,11 @@
 import random
 
-def get_numbers_ticket(min, max, quantity):
-    if min < 1 or max > 1000 or min >= max or quantity > (max - min):
-        return []
-    range_choice = random.sample(range(min, max + 1), quantity)
-    return sorted(range_choice)
+def get_numbers_ticket(min, max, quantity):# Оголошення функції
+    if min < 1 or max > 1000 or min >= max or quantity > (max - min): # Перевірка коректності данних
+        return [] #Повертає порожній список, при некоректному вводі
+    range_choice = random.sample(range(min, max + 1), quantity) #Випадковий вибір чисел
+    return sorted(range_choice) #Повертає відсортований список в порядку зростання
     
 
 
-print(get_numbers_ticket(0, 1000, 6))
+print(get_numbers_ticket(10, 15, 9)) # Виклклик функції з перевіркою умови і виводом результату
