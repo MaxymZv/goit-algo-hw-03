55d88deb82163042b5b558b6359e3d4fb158e112
Maxym Zviahin <maxzvyagin@gmail.com>
2025-04-04 00:48:28 +0300

    deleted_some_garbage_and_correctly_converted_to_int

--- homework.py
+++ homework.py
@@ -1,17 +1,16 @@
-from datetime import datetime, timedelta
+from datetime import datetime
 
 def get_days_from_today(date):
     try: #перевіряє коректність введених даних
-       date_insert = "date" #Преретворює в рядок
        date_from = datetime.strptime(date, "%Y-%m-%d") #Перетворюємо в зручний формат
        today_date = datetime.now() #Отримуємо сьогоднішню дату
        days_difference = (today_date - date_from).days #Бере різницю між датами
-       change_to_integer = int(days_difference) #Перетворює в ціле число
-       return f"Різниця між датами складає {days_difference} днів" #Повертає результат
+       return int(days_difference) #Повертає результат та Перетворює в ціле число
     except ValueError: #Перевіряє чи введена дата у правильному форматі
         return "Некоректний формат дати. Використовуйте YYYY-MM-DD."
     except TypeError:  #Перевіряє чи введена дата є рядком
         return "Некоректний тип даних. Використовуйте рядок."
 
-#Викликликає функцію та виводе на екран резултат
-print(get_days_from_today("2023-10-01")) 
+
+
+print(f"різниця днів складає: {get_days_from_today("2023-10-01")}") #Викликликає функцію та виводе на екран резултат
